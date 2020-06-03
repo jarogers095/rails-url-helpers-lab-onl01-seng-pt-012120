9e00b0b3f7cfa621b79b2fde4157a886c088c5e4
James Rogers <jarogers095@gmail.com>
2020-06-03 19:43:15 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :students, only: [:index, :show, :edit]
+  resources :students, only: [:index, :show]
+  get '/students/:id/edit', to: 'students#edit', as: 'activate'
 end
